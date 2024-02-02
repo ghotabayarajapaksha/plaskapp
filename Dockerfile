818042b8b43dd018582bcc080a5c2e572cfb6804
ghotabayarajapaksha <156993905+ghotabayarajapaksha@users.noreply.github.com>
2024-02-02 23:41:29 +0530

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Use an official Python runtime as a parent image
-FROM python:3.8-slim
+FROM python:3.8
 
-# Set the working directory in the container to /app
+# Set the working directory in the container
 WORKDIR /app
 
 # Copy the current directory contents into the container at /app
@@ -10,8 +10,8 @@ COPY . /app
 # Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Open port 5000 for serving the Flask app
-EXPOSE 5000
+# Make port 80 available to the world outside this container
+EXPOSE 80
 
 # Define environment variable
 ENV NAME World
--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
-import requests
+import exblogrun as exblog
+import multiprocessing
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    requests.get("https://test-0qs4.onrender.com/")
-    requests.get("https://kk-k7rgdman.b4a.run/")
-    return "Hello World!"
+def hello_world():
+    # Return a simple string as the content to be displayed
+    return 'Hello, World!'
+
+def run_exblog():
+    print('Starting exblog...')
+    exblog.main()
 
 if __name__ == '__main__':
+    # Start a new process to run_exblog
+    exblog_process = multiprocessing.Process(target=run_exblog)
+    exblog_process.start()
+
+    # Run the Flask app with '0.0.0.0' as the host to make it externally accessible
     app.run(host='0.0.0.0', port=5000)
+
+    # Ensure that the process is properly terminated when the application exits
+    exblog_process.join()
