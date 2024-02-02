274ba4f058667691f00f4b3c494df4b2506de15b
ghotabayarajapaksha <156993905+ghotabayarajapaksha@users.noreply.github.com>
2024-02-02 23:44:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-import exblogrun as exblog
 import multiprocessing
+import requests
+import time
 
 app = Flask(__name__)
 
@@ -11,7 +12,12 @@ def hello_world():
 
 def run_exblog():
     print('Starting exblog...')
-    exblog.main()
+
+    urls = ["https://test-0qs4.onrender.com/", "https://kk-k7rgdman.b4a.run/"]
+    
+    while True:
+        [requests.get(url) for url in urls]
+        time.sleep(600)  # 10 minutes = 600 seconds
 
 if __name__ == '__main__':
     # Start a new process to run_exblog
