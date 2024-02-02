209c21e67b27be4220a53873ec4cf74444dba635
ghotabayarajapaksha <156993905+ghotabayarajapaksha@users.noreply.github.com>
2024-02-02 23:32:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    requests.get("https://test-0qs4.onrender.com/")
+    requests.get("https://kk-k7rgdman.b4a.run/")
     return "Hello World!"
 
 if __name__ == '__main__':
